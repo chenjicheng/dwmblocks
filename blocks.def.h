c8cb7e44e74f74795e45d173119cca34a7cd0778
Chen Jicheng <git@chenjicheng.com>
2022-05-05 22:31:59 +0800

    feat: Add volume status

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sb-volume",							0,		10}
+
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%Y-%m-%d %A %H:%M'",					5,		0},
