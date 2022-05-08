abbc50fa41ae3038dc9426d851cb25a8f46c974e
Chen Jicheng <git@chenjicheng.com>
2022-05-08 11:01:14 +0800

    fix: Add missing comma

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-volume",							0,		10}
+	{"", "sb-volume",							0,		10},
 
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
